88f2fa068b7151857db214418db7c1a4519125c4
f.collas@autobiz.com <f.collas@autobiz.com>
2019-03-18 16:28:14 +0100

    adapted main to new config paths and cleaned some project folders

--- application/main.py
+++ application/main.py
@@ -11,7 +11,7 @@ def main(market):
     # read all environnement and config
 
     load_dotenv()
-    with open("/usr/local/src/config/" + market + '_' + country_ + '_config.json', 'r') as f:
+    with open("/usr/local/src/config/final_config/" + market + '_' + country_ + '_' + website_ + '_config.json', 'r') as f:
         config = json.load(f)
     context = config['GENERAL']['context']
 
