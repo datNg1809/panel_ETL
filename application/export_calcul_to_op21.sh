51102e5bf28e7a24157c325653a93fd21c764b23
Dat Nguyen <d.nguyen@autobiz.com>
2019-07-24 13:00:45 +0200

    add parsing script & bash script to download csv from Calcul

---
+++ application/export_calcul_to_op21.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+ssh -l d.nguyen -L 2222:127.0.0.1:22 -4 tele@46.105.109.54  -fN
+#scp -P 2222 tele@127.0.0.1:/home/livraison_user/upload/AUTO_AU/CARSALES_* /home/d.nguyen/panel/test/
+
+
+FOLDER_HOST=$1
+PATTERN=$2
+YYYY=${PATTERN:0:4}
+MM=${PATTERN:5:2}
+DIR=$3
+
+#Sample call
+# bash /home/d.nguyen/panel/application/export_calcul_to_op21.sh IMMO_DE 2019_07 /home/d.nguyen/panel/
+
+
+
+LOGDIR=$DIR/application
+LOG=$LOGDIR/Export_${PATTERN}.log
+TEMPDIR=$DIR/application/temporary
+[[ -e $TEMPDIR ]] || mkdir -p $TEMPDIR
+DESTDIR=$DIR/data/input
+
+scp -P 2222 tele@127.0.0.1:/home/livraison_user/upload/${FOLDER_HOST}/*${PATTERN}* ${TEMPDIR}
+gunzip -r ${TEMPDIR}
+
+for file in ${TEMPDIR}/*
+do
+    if [[ "DOUBLON" == *${file}* ]] || [[ "_TEL_" == *${file}* ]]; then
+        continue
+    fi
+
+    cat file | python ${LOGDIR}/parsing.py ${FOLDER_HOST} ${PATTERN} ${DESTDIR}
+
+    rsync $TEMPDIR/file $DESTDIR/
+    touch $LOG
+    echo "At time {$date}, exporting data $file into dir=$DESTDIR" >> $LOG
+done
+rm -rf $TEMPDIR
+
+#|sed "s/'/\'/;s/\",\"/\";\"/g;s/\t/\";\"/g;s/^/\"/;s/$/\"/;s/\n//g;s/NULL//g"
---
+++ application/parsing.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+import csv
+import os
+from io import StringIO
+
+
+
+
+with open(sys.stdin, 'r') as csvfile:
+    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+
+if not sys.stdin.isatty():
+    file = sys.stdin.readlines()
+file = ''.join(file)
+file = StringIO(file)
+
+
+df= pd.read_csv(file, sep=dialect, enconding="utf-8")
+
+webs= df.SITE.value_counts().index.tolist()
+for web in webs:
+    outfile = df[df['SITE'] == web]
+    out_name= sys.argv[1] + "_" + str(web).upper() + "_" + sys.argv[2] + ".csv"
+    output=os.path.join(sys.argv[3] + out_name)
+    outfile.to_csv(output, sep=';', index=False, encoding = 'utf-8')
