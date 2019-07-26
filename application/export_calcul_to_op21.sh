1dbd9b53f6e9c8ddd3b9a9e1a72d5d21d0543c15
Dat Nguyen <d.nguyen@autobiz.com>
2019-07-26 14:56:52 +0200

    set parsing.py run out side of docker

--- application/export_calcul_to_op21.sh
+++ application/export_calcul_to_op21.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-ssh -l d.nguyen -L 2222:127.0.0.1:22 -4 tele@46.105.109.54  -fN
-#scp -P 2222 tele@127.0.0.1:/home/livraison_user/upload/AUTO_AU/CARSALES_* /home/d.nguyen/panel/test/
-
 
 FOLDER_HOST=$1
 PATTERN=$2
@@ -13,33 +10,49 @@ DIR=$3
 #Sample call
 # bash /home/d.nguyen/panel/application/export_calcul_to_op21.sh IMMO_DE 2019_07 /home/d.nguyen/panel/
 
-
-
-LOGDIR=$DIR
-LOG=$LOGDIR/Export_${PATTERN}.log
-TEMPDIR=$DIR/temporary
+LOGDIR=$DIR/application
+LOG=$LOGDIR/Export_${FOLDER_HOST}_${PATTERN}.log
+TEMPDIR=$DIR/application/temporary_${FOLDER_HOST}_${PATTERN}
 [[ -e $TEMPDIR ]] || mkdir -p $TEMPDIR
-DESTDIR=/data/input
+DESTDIR=$DIR/data/input
 
-scp -P 2222 tele@127.0.0.1:/home/livraison_user/upload/${FOLDER_HOST}/*${PATTERN}* ${TEMPDIR}
-gunzip -rf ${TEMPDIR}
+#transfer function
+transfer () {
+  ssh -l d.nguyen -L 2222:127.0.0.1:22 -4 tele@46.105.109.54  -fN
+  scp -P 2222 tele@127.0.0.1:/home/livraison_user/upload/${FOLDER_HOST}/*${PATTERN}* ${TEMPDIR}
+  gunzip -rf ${TEMPDIR}
 
-for file in ${TEMPDIR}/*.csv
-do
-    if [[ ${file} == *"DOUBLON"* ]] || [[ ${file} == *"_TEL_"* ]]; then
-        continue
+  for file in ${TEMPDIR}/*.csv
+  do
+      if [[ ${file} == *"DOUBLON"* ]] || [[ ${file} == *"_TEL_"* ]]; then
+          continue
+      fi
+
+      cat ${file} | python3 $DIR/application/parsing.py ${FOLDER_HOST} ${PATTERN} ${TEMPDIR}
+      rsync $file $DESTDIR/
+      current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
+      touch $LOG
+
+      echo "$current_date_time : Exporting $file into dir=$DESTDIR" >> $LOG
+
+  done
+  rm -rf $TEMPDIR
+}
+
+#check if data is transferred already
+if [[ -e $LOG ]] && [[ ${FOLDER_HOST:0:2} == "VO" ]]; then
+    line= tail -n 1 $LOG
+    datelog= ${line:0:10}
+    datelog= ${date --date=$datelog +%F}
+    current_date_time=${date + "%Y-%m-%d"}
+    if [[ $current_date_time == $(datelog -d "+3 days") ]]; then
+      transfer
+    else
+      exit 0
     fi
+elif [[ ! -e $LOG ]]; then
+  transfer
+fi
 
-    cat ${file} | python3 /usr/local/src/parsing.py ${FOLDER_HOST} ${PATTERN} ${TEMPDIR}
-
-
-    rsync $file $DESTDIR/
-    current_date_time="`date "+%Y-%m-%d %H:%M:%S"`"
-    touch $LOG
-
-    echo "Exporting $file into dir=$DESTDIR at time {$current_date_time}" >> $LOG
-
-done
-rm -rf $TEMPDIR
 
 #|sed "s/'/\'/;s/\",\"/\";\"/g;s/\t/\";\"/g;s/^/\"/;s/$/\"/;s/\n//g;s/NULL//g"
--- application/parsing.py
+++ application/parsing.py
@@ -1,27 +1,29 @@
-import pandas as pd
+#import pandas as pd
 import sys
 import csv
 import os
 from io import StringIO
+from itertools import groupby
 
+"""
+sys.argv[1]: folder name ~ market_country, for ex, IMMO_DE
+sys.argv[2]: time pattern, for ex, 2019_07
+sys.argv[3]: directory to save output, here is /home/d.nguyen/panel/data/input
+"""
 
+os.chdir(sys.argv[3])
 
+#csvfile=sys.stdin.read().splitlines()
+#dialect = csv.Sniffer().sniff(csvfile.read(1024))
+df = csv.reader(sys.stdin)
+header = next(df)
+site_index=header.index("SITE")
 
-with open(sys.stdin, 'r') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.read(1024))
+groupfunc = lambda row: row[site_index]
 
-if not sys.stdin.isatty():
-    file = sys.stdin.readlines()
-file = ''.join(file)
-file = StringIO(file)
-
-
-df= pd.read_csv(file, sep=dialect, enconding="utf-8")
-
-webs= df.SITE.value_counts().index.tolist()
-for web in webs:
-    outfile = df[df['SITE'] == web]
-    out_name= sys.argv[1] + "_" + str(web).upper() + "_" + sys.argv[2] + ".csv"
-    output=os.path.join(sys.argv[3] + out_name)
-    outfile.to_csv(output, sep=';', index=False, encoding = 'utf-8')
-    sys.stdout = open(outfile, 'w')
+for key, rows in groupby(sorted(df, key=groupfunc), groupfunc):
+    with open(sys.argv[1] + "_"+ key.upper() + "_" + sys.argv[2] + ".csv", "w") as output:
+        cw = csv.writer(output,delimiter=";")
+        cw.writerow([g for g in header])
+        cw.writerows(rows)
+print("writing completed")
